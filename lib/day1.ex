3027b5134b65044b6609d1f52923984659989e02
Md. Al Amin <alamin.ece.ku@gmail.com>
2020-03-16 00:45:07 +0600

    Day 1 challenge

--- lib/day1.ex
+++ lib/day1.ex
@@ -3,15 +3,31 @@ defmodule Fuel do
   Day 1 problem in `Advent of Code`.
   """
 
-  def calculate do
+  def calculate_fuel do
     Reader.readdata("d1")
     |> String.split("\r\n")
-    |> Enum.map(fn x -> process(x) end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&(div(&1, 3) - 2))
     |> Enum.reduce(fn x, acc -> x + acc end)
   end
 
-  defp process(num_string) do
-    {num, _} = Integer.parse(num_string)
-    div(num, 3) - 2
+  def calculate_total_fuel do
+    Reader.readdata("d1")
+    |> String.split("\r\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&(fuel_requirement(&1)))
+    |> Enum.reduce(fn x, acc -> x + acc end)
   end
+
+  def fuel_requirement(mass) do
+    (div(mass, 3) - 2)
+     |> fuel_req()
+  end
+
+  defp fuel_req(0), do: 0
+  defp fuel_req(x) when x < 0, do: 0
+  defp fuel_req(x) do 
+    x + fuel_req(div(x, 3) - 2)
+  end
+
 end
