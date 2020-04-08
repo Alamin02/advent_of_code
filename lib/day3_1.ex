1313ff38ab156256c9b3a47afe101d65c40dfd81
Md. Al Amin <alamin.ece.ku@gmail.com>
2020-04-08 20:42:30 +0600

    Minor refactor

--- lib/day3_1.ex
+++ lib/day3_1.ex
@@ -1,4 +1,6 @@
 defmodule CrossedWires do
+  @spec calculate :: any
+
   def calculate do
     Reader.read_data("d3")
     |> String.split("\r\n")
@@ -6,45 +8,42 @@ defmodule CrossedWires do
     |> run()
   end
 
+  @spec get_direction_and_length(binary) :: {integer, any}
+
   def get_direction_and_length(x) do
     <<direction::utf8>> <> length = x
-
     {direction, String.to_integer(length)}
   end
+
+  @spec process_list(any) :: [any]
 
   def process_list(list) do
     Enum.map(list, fn x -> get_direction_and_length(x) end)
   end
 
+  @spec process_instruction(any, any) :: any
+
   def process_instruction([{?R, length} | rest], data) do
     {x, y} = List.last(data)
-
     new_data = data ++ for n <- (x + 1)..(x + length), do: {n, y}
-
     process_instruction(rest, new_data)
   end
 
   def process_instruction([{?U, length} | rest], data) do
     {x, y} = List.last(data)
-
     new_data = data ++ for n <- (y + 1)..(y + length), do: {x, n}
-
     process_instruction(rest, new_data)
   end
 
   def process_instruction([{?L, length} | rest], data) do
     {x, y} = List.last(data)
-
     new_data = data ++ for n <- (x - 1)..(x - length), do: {n, y}
-
     process_instruction(rest, new_data)
   end
 
   def process_instruction([{?D, length} | rest], data) do
     {x, y} = List.last(data)
-
     new_data = data ++ for n <- (y - 1)..(y - length), do: {x, n}
-
     process_instruction(rest, new_data)
   end
 
@@ -52,9 +51,13 @@ defmodule CrossedWires do
     data
   end
 
+  @spec find_intersections(any, any) :: MapSet.t(any)
+
   def find_intersections(list1, list2) do
     list1 |> MapSet.new() |> MapSet.intersection(MapSet.new(list2))
   end
+
+  @spec run(any) :: any
 
   def run(data) do
     list1 = Enum.at(data, 0)
