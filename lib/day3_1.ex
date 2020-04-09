e3c28afc75e785aed35451c4102ab35cd35dfa23
alamin02 <alamin.ece.ku@gmail.com>
2020-04-09 17:35:42 +0600

    Refactor code

--- lib/day3_1.ex
+++ lib/day3_1.ex
@@ -1,28 +1,23 @@
 defmodule CrossedWires do
-  @spec calculate :: any
 
-  def calculate do
+  @spec process_input :: [any]
+  def process_input do
     Reader.read_data("d3")
-    |> String.split("\r\n")
+    |> String.split("\n")
     |> Enum.map(&String.split(&1, ","))
-    |> run()
   end
 
   @spec get_direction_and_length(binary) :: {integer, any}
-
-  def get_direction_and_length(x) do
-    <<direction::utf8>> <> length = x
+  def get_direction_and_length(<<direction::utf8>> <> length) do
     {direction, String.to_integer(length)}
   end
 
   @spec process_list(any) :: [any]
-
   def process_list(list) do
     Enum.map(list, fn x -> get_direction_and_length(x) end)
   end
 
   @spec process_instruction(any, any) :: any
-
   def process_instruction([{?R, length} | rest], data) do
     {x, y} = List.last(data)
     new_data = data ++ for n <- (x + 1)..(x + length), do: {n, y}
@@ -52,18 +47,16 @@ defmodule CrossedWires do
   end
 
   @spec find_intersections(any, any) :: MapSet.t(any)
-
   def find_intersections(list1, list2) do
     list1 |> MapSet.new() |> MapSet.intersection(MapSet.new(list2))
   end
 
-  @spec run(any) :: any
+  @spec run :: any
+  def run do
+    data = process_input()
 
-  def run(data) do
-    list1 = Enum.at(data, 0)
-    list2 = Enum.at(data, 1)
-    processed_list1 = process_list(list1) |> process_instruction([{0, 0}])
-    processed_list2 = process_list(list2) |> process_instruction([{0, 0}])
+    processed_list1 = Enum.at(data, 0) |> process_list |> process_instruction([{0, 0}])
+    processed_list2 = Enum.at(data, 1) |> process_list |> process_instruction([{0, 0}])
 
     find_intersections(processed_list1, processed_list2)
     |> Enum.filter(fn {x, y} -> !(x === 0 && y === 0) end)
